d9e7a7c1c767f52a9d5e083e0a4cb887c4b35631
Andrea Serboli <andrea.serboli12@gmail.com>
2020-11-12 16:11:42 +0100

    mail completa

--- Gioco dei dadi/js/dadi.js
+++ Gioco dei dadi/js/dadi.js
@@ -1,2 +1,4 @@
 // Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
+var numeroUtente = Math.random() *6 + 1;
+console.log(numeroUtente)
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
--- Mail/js/Mail.js
+++ Mail/js/Mail.js
@@ -4,17 +4,21 @@ console.log(mail)
 var mailUtente = prompt('Qual è la tua mail?')
 console.log(mailUtente)
 // controlla che sia nella lista di chi può accedere,
+var risultato = false;
 for(var i = 0 ; i < mail.length; i++) {
 
   if (mailUtente == mail[i]) {
-    console.log(mail[i])
-    alert('CHE FORTUNA! Puoi accedere al sito.');
-  } else if (mailUtente != [i])  {
-    console.log(mail[i])
-    alert('MI DISPIACE! Non puoi accedere al sito.');
+    risultato = true;
   }
 
 }
+
+if (risultato === true) {
+  alert('CHE FORTUNA! Puoi accedere al sito.');
+} else {
+  alert('MI DISPIACE! Non puoi accedere al sito.');
+}
+
 
 
 // stampa un messaggio appropriato sull’esito del controllo.
