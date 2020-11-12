40e4c4f3ececa7366420bfc9624ab5e675ec6587
Andrea Serboli <andrea.serboli12@gmail.com>
2020-11-12 16:46:12 +0100

    gioco dadi

--- Gioco dei dadi/js/dadi.js
+++ Gioco dei dadi/js/dadi.js
@@ -1,4 +1,22 @@
 // Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
-var numeroUtente = Math.random() *6 + 1;
+var numeroUtente = Math.floor(Math.random() *6 + 1);
+document.getElementById('dadoUtente').innerHTML =
+numeroUtente
 console.log(numeroUtente)
+
+var numeroPC = Math.floor(Math.random() *6 + 1);
+document.getElementById('dadoPC').innerHTML =
+numeroPC
+console.log(numeroPC)
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+if (numeroUtente > numeroPC) {
+  document.getElementById('risultato').innerHTML =
+  'HAI VINTO'
+} else if (numeroUtente < numeroPC) {
+  document.getElementById('risultato').innerHTML =
+  'HAI PERSO'
+} else if (numeroUtente = numeroPC) {
+  document.getElementById('risultato').innerHTML =
+  'PAREGGIO'
+}
